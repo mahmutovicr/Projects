e430c18f1481eef3ecfc4298671caa1a072af6f0
Rahman Mahmutović <247497581+mahmutovicr@users.noreply.github.com>
2026-03-07 18:13:54 +0100

    Update index.php

--- log-in-form/index.php
+++ log-in-form/index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION["user"])) {
-  header("Location: login.php");
+    header("Location: login.php");
+    exit();
 }
 ?>
 <!DOCTYPE html>
@@ -9,16 +10,15 @@ if (!isset($_SESSION["user"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>User Dashboard</title>
 </head>
 <body>
-    <div class="container">
-     <h1>Welcome</h1>
-     <a href="Logout.php" class="btn btn-warning">Log out</a>   
-     </div>
-    
+<div class="container">
+    <h1>Welcome</h1>
+    <a href="logout.php" class="btn btn-warning">Log out</a>
+</div>
 </body>
 </html>
